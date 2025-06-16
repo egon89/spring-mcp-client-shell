bacd1b0f969cdf6f4f6ddbf402d58a20bbcbff97
Everton Goncalves <everton.goncalves.89@gmail.com>
2025-06-16 00:39:24 -0300

    wip: tools

--- src/main/java/com/egon89/mcp_client/ChatConfig.java
+++ src/main/java/com/egon89/mcp_client/ChatConfig.java
@@ -1,10 +1,13 @@
 package com.egon89.mcp_client;
 
+import java.util.Arrays;
+
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.tool.ToolCallbackProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,8 +20,13 @@ public class ChatConfig {
     }
 
     @Bean
-    public ChatClient chatClient(ChatModel chatModel, ChatMemory chatMemory) {
+    public ChatClient chatClient(ChatModel chatModel, ChatMemory chatMemory, ToolCallbackProvider tools) {
+        System.out.println("ChatClient initialized with the following tools:");
+        Arrays.stream(tools.getToolCallbacks())
+            .forEach(tool -> System.out.printf("> Tool: %s\n", tool.getToolDefinition().name()));
+
         return ChatClient.builder(chatModel)
+            .defaultToolCallbacks(tools)
             .defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory).conversationId("shell").build())
             .build();
     }
